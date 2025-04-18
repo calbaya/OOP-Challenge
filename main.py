adffe5bde1a5898e923b96bd700b1ec086044f9d
calbaya <bayacaleb2@gmail.com>
2025-04-18 18:10:17 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+my_pet = Pet("Bosco")
+
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.get_status()
+
+# bonus
+my_pet.train("sit")
+my_pet.train("roll over")
+my_pet.show_tricks()
---
+++ pet.py
@@ -0,0 +1,43 @@
+class Pet:
+    def __init__(self, name):
+        self.name = "Bosco"
+        self.hunger = 5
+        self.energy = 5
+        self.happiness = 5
+        self.tricks = []
+
+    def eat(self):
+        self.hunger = max(self.hunger - 3, 0)
+        self.happiness = min(self.happiness + 1, 10)
+        print(f"{self.name} just ate his favourite meal!")
+
+    def sleep(self):
+        self.energy = min(self.energy + 5, 10)
+        print(f"{self.name} is now well rested, added more energy!")
+
+    def play(self):
+        self.energy = max(self.energy - 2, 0)
+        self.happiness = min(self.happiness + 2, 10)
+        self.hunger = min(self.hunger + 1, 10)
+        print(f"{self.name} had fun playing, literaly had the best time of his life!")
+
+    def get_status(self):
+        print(f" {self.name}'s Status:")
+        print(f"  Hunger: {self.hunger}/10")
+        print(f"  Energy: {self.energy}/10")
+        print(f"  Happiness: {self.happiness}/10")
+
+
+        # Bonus 
+    def train(self, trick):
+        if trick not in self.tricks:
+            self.tricks.append(trick)
+            print(f"{self.name} learned a new trick: {trick}!")
+        else:
+            print(f"{self.name} already knows how to {trick}.")
+
+    def show_tricks(self):
+        if self.tricks:
+            print(f"{self.name} knows the following tricks: {', '.join(self.tricks)}")
+        else:
+            print(f"{self.name} hasn't learned any tricks yet.")
